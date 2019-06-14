e8da06d179f703a1a63d589e0ae33355f4113f67
tamurkhan <t.khan7@wlv.ac.uk>
2019-06-14 23:46:47 +0100

    smooth scrolling jquery

--- js/main.js
+++ js/main.js
@@ -11,27 +11,18 @@ function initMap() {
   const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
-// // Sticky menu background
-// window.addEventListener("scroll", function() {
-//   if (window.scrollY > 150) {
-//     document.querySelector("#navbar").style.opacity = 0.9;
-//   } else {
-//     document.querySelector("#navbar").style.opacity = 1;
-//   }
-// });
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
 
-// // Smooth Scrolling
-// $("#navbar a, .btn").on("click", function(event) {
-//   if (this.hash !== "") {
-//     event.preventDefault();
+    const hash = this.hash;
 
-//     const hash = this.hash;
-
-//     $("html, body").animate(
-//       {
-//         scrollTop: $(hash).offset().top - 100
-//       },
-//       800
-//     );
-//   }
-// });
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      900
+    );
+  }
+});
